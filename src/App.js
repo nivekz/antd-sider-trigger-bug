da22131006b2bbdbfdbde70b799f57bce1d7c805
Kevin Zhao <klzhao@gmail.com>
2019-06-18 18:16:06 +0800

    Add code to show bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Icon, Layout, List, Menu } from 'antd';
 import './App.css';
+
+const { Content, Footer, Sider } = Layout;
+
+const data = [
+  'hello',
+  'hello',
+  'hello',
+  'hello',
+  'hello',
+  'hello',
+  'hello',
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={broken => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item key="1">
+            <Icon type="user" />
+            <span className="nav-text">nav 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="video-camera" />
+            <span className="nav-text">nav 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="upload" />
+            <span className="nav-text">nav 3</span>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Icon type="user" />
+            <span className="nav-text">nav 4</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        {/* <Header style={{ background: '#fff', padding: 0 }} /> */}
+        <Content style={{ margin: '24px 16px 0' }}>
+          <List
+            itemLayout="horizontal"
+            dataSource={data}
+            // pagination={pagination}
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  title={item}
+                  description={item}
+                />
+              </List.Item>
+            )}
+          />
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
